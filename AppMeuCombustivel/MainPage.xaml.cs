81ec9c77a7ca9327455029d5b78ecf227a5ce054
vboxuser <vboxuser@WINDOWS>
2026-03-02 10:29:10 -0300

    Adicionar arquivos de projeto.

---
+++ AppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace AppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double preco_etanol = Convert.ToDouble(etanol.Text);
+            double preco_gasolina = Convert.ToDouble(gasolina.Text);
+            string msg;
+
+            if(preco_etanol > (preco_gasolina*0.7))
+            {
+                msg = "Compensa gasolina";
+            }
+            else
+            {
+                msg = "Compensa o etanol";
+            }
+            DisplayAlertAsync("Resultado:", msg, "OK");
+        }
+    }
+}
